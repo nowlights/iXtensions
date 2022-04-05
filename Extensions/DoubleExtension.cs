73857c9cf08e1bdfa2593cc1bed512fb04e93ca0
Junior Dias <57915897+nowlights@users.noreply.github.com>
2022-04-05 19:55:17 -0300

    Update double extension and add two methods

--- Extensions/DoubleExtension.cs
+++ Extensions/DoubleExtension.cs
@@ -2,7 +2,24 @@ namespace iXtentions.Extensions
 {
     public static class DoubleExtension
     {
-         public static double ToMathRound(this double Value)
+         /// <summary>
+        /// Always round to the nearest / Arredonda sempre para o proximo
+        /// </summary>
+        public static double ToMathRound(this double Value)
             => Math.Round(Value);
+
+
+        /// <summary>
+        /// Always round down / Arredonda sempre para baixo
+        /// </summary>
+        public static double ToMathFloor(this double Value)
+            => Math.Floor(Value);
+
+
+        /// <summary>
+        /// Always round up / Arredonda sempre para cima
+        /// </summary>
+        public static double ToMathCeiling(this double Value)
+            => Math.Ceiling(Value);
     }
 }
