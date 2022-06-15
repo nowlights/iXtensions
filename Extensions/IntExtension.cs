09fc4866e4bfa88597261c9ac431a937e569de4e
nowlights <j.junior1197@hotmail.com>
2022-06-15 16:59:09 -0300

    int service relocated to IntExtension, Add SimilarityTool

--- Extensions/IntExtension.cs
+++ Extensions/IntExtension.cs
@@ -33,6 +33,20 @@ namespace iXtensions.Extensions
         }
 
 
+        /// <summary>
+        /// Get a bool if number x is divisible by a number N for example:
+        /// x = 1 : n = 4 ? 1 is divisible by 4? False
+        /// x = 2 : n = 4 ? 2 is divisible by 4? False
+        /// x = 4 : n = 4 ? 4 is divisible by 4? True
+        /// x = 12 : n = 4 ? 12 is divisible by 4? True ...
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="n"></param>
+        /// <returns>Return a bool is x is divisible for a N</returns>
+        public static bool IsDisivisible(int x, int n)
+            => (x % n) == 0;
+
+
 
 
     }
